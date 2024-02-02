ea77e1a1abdb98d0757b248811f392f20888d139
alonzo Alan <vamshi5070k@gmail.com>
2024-02-02 20:49:54 +0530

    Add linear search

---
+++ search/linear_search.py
@@ -0,0 +1,17 @@
+def linear_search(elements,key):
+    index = 0
+    for element in elements:
+        if key == element:
+            return index 
+        index += 1
+    return -1
+
+#print(linear_search([1,2,3],2))
+def test_lsearch():
+    assert linear_search([1,2,3,4,5,6],1) == 0, "Should be 0"
+    assert linear_search([1,2,3,4,5,6],6) == 5, "Should be 5"
+    assert linear_search([],1) == -1, "Should be -1"
+
+if __name__==  "__main__":
+    test_lsearch()
+    print("Linear search passed")
